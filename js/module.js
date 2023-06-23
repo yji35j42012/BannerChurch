f14ce4268c020107c09c11b5b6f8809c05ffde72
yji35j4212 <yji35j4212@gmail.com>
2023-06-23 11:45:00 +0800

    檔案同步

--- js/module.js
+++ js/module.js
@@ -1,12 +1,11 @@
 window.addEventListener("scroll", scrollListener);
 function scrollListener() {
-    if (window.pageYOffset == 0) {
-        document.body.classList.remove("scroll");
-    } else {
-        document.body.classList.add("scroll");
-    }
+	if (window.pageYOffset == 0) {
+		document.body.classList.remove("scroll");
+	} else {
+		document.body.classList.add("scroll");
+	}
 }
-
 
 // nav滑動
 window.addEventListener("scroll", scrollListener);
@@ -14,78 +13,95 @@ var nav = document.querySelectorAll("#nav > a");
 var scrollState = "down";
 var oldScroll = 0;
 
-var nowHref = location.href
+var nowHref = location.href;
 
-console.log('!!!!!!', nowHref.split('#').length);
-if (nowHref.split('#').length == 2) {
-   setTimeout(() => {
-    let moveItem = nowHref.split('#')[1];
-    var item = document.querySelector("#" + moveItem);
-    var goScroll = item.offsetTop - 71;
-    var nowScroll = window.scrollY;
-    nowScroll >= oldScroll ? (scrollState = "down") : (scrollState = "up");
-    oldScroll = nowScroll;
+if (nowHref.split("#").length == 2) {
+	setTimeout(() => {
+		let moveItem = nowHref.split("#")[1];
+		var item = document.querySelector("#" + moveItem);
+		var goScroll = item.offsetTop - 71;
+		var nowScroll = window.scrollY;
+		nowScroll >= oldScroll ? (scrollState = "down") : (scrollState = "up");
+		oldScroll = nowScroll;
 
-    if (goScroll > nowScroll) {
-        goDown(nowScroll, goScroll);
-        scrollState = "down";
-    } else {
-        goTop(nowScroll, goScroll);
-        scrollState = "up";
-    }
-   }, 50);
+		if (goScroll > nowScroll) {
+			goDown(nowScroll, goScroll);
+			scrollState = "down";
+		} else {
+			goTop(nowScroll, goScroll);
+			scrollState = "up";
+		}
+	}, 50);
 }
 
-
-
-
 for (let i = 0; i < nav.length; i++) {
-    const element = nav[i];
-    element.onclick = function () {
-        let moveItem = element.getAttribute("name");
-        if (nowHref.indexOf("news.html") !== -1) {
-            console.log('!!!', moveItem);
-            location.href = './index.html#' + moveItem
-        } else {
-            var item = document.querySelector("#" + moveItem);
-            var goScroll = item.offsetTop - 71;
-            var nowScroll = window.scrollY;
-            nowScroll >= oldScroll ? (scrollState = "down") : (scrollState = "up");
-            oldScroll = nowScroll;
+	const element = nav[i];
+	element.onclick = function() {
+		let moveItem = element.getAttribute("name");
+		if (
+			nowHref.indexOf("news.html") !== -1 ||
+			nowHref.indexOf("news_detail.html") !== -1
+		) {
+			console.log("!!!", moveItem);
+			location.href = "./index.html#" + moveItem;
+		} else {
+			var item = document.querySelector("#" + moveItem);
+			var goScroll = item.offsetTop - 71;
+			var nowScroll = window.scrollY;
+			nowScroll >= oldScroll
+				? (scrollState = "down")
+				: (scrollState = "up");
+			oldScroll = nowScroll;
 
-            if (goScroll > nowScroll) {
-                goDown(nowScroll, goScroll);
-                scrollState = "down";
-            } else {
-                goTop(nowScroll, goScroll);
-                scrollState = "up";
-            }
-        }
-
-    };
+			if (goScroll > nowScroll) {
+				goDown(nowScroll, goScroll);
+				scrollState = "down";
+			} else {
+				goTop(nowScroll, goScroll);
+				scrollState = "up";
+			}
+		}
+	};
 }
 
 function goTop(from, to) {
-    scrollState = "up";
-    let scrollTime = setInterval(() => {
-        if (from <= to) {
-            from = to;
-            clearInterval(scrollTime);
-        } else {
-            window.scrollTo(0, from);
-            from = from - 50;
-        }
-    }, 0);
+	scrollState = "up";
+	let scrollTime = setInterval(() => {
+		if (from <= to) {
+			from = to;
+			clearInterval(scrollTime);
+		} else {
+			window.scrollTo(0, from);
+			from = from - 50;
+		}
+	}, 0);
 }
 function goDown(from, to) {
-    let scrollTime = setInterval(() => {
-        if (from >= to) {
-            from = to;
-            clearInterval(scrollTime);
-        } else {
-            window.scrollTo(0, from);
-            from = from + 50;
-        }
-    }, 0);
+	let scrollTime = setInterval(() => {
+		if (from >= to) {
+			from = to;
+			clearInterval(scrollTime);
+		} else {
+			window.scrollTo(0, from);
+			from = from + 50;
+		}
+	}, 0);
 }
 // nav滑動
+
+var news_item = document.querySelectorAll("[name='news_item']");
+if (news_item) {
+	for (let i = 0; i < news_item.length; i++) {
+		const element = news_item[i];
+		element.onclick = function() {
+			location.href = "news_detail.html";
+		};
+	}
+}
+
+var page_detail_all = document.querySelector("#page_detail_all");
+if (page_detail_all) {
+	page_detail_all.onclick = function() {
+		location.href = "news.html";
+	};
+}
